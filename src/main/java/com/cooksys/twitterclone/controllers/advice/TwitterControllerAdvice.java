d4d5a5ad04d46c3f66b7f1ae0c880e989a469600
Karanvir Sandhu <k2sandhu@ucsd.edu>
2023-08-28 14:49:21 -0700

    Update to files

--- src/main/java/com/cooksys/twitterclone/controllers/advice/TwitterControllerAdvice.java
+++ src/main/java/com/cooksys/twitterclone/controllers/advice/TwitterControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.cooksys.twitterclone.controllers.advice;
 
-import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,18 +12,18 @@ import org.springframework.web.client.HttpClientErrorException.BadRequest;
 import com.cooksys.twitterclone.dtos.ErrorDto;
 import com.cooksys.twitterclone.exceptions.BadRequestException;
 
-import javassist.NotFoundException;
+//import javassist.NotFoundException;
 public class TwitterControllerAdvice {
-
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(BadRequest.class)
-	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
-		return new ErrorDto(badRequestException.getMessage());
-	}
-
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(NotFoundException.class)
-	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
-		return new ErrorDto(notFoundException.getMessage());
-	}
+//
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	@ExceptionHandler(BadRequest.class)
+//	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+//		return new ErrorDto(badRequestException.getMessage());
+//	}
+//
+//	@ResponseStatus(HttpStatus.NOT_FOUND)
+//	@ExceptionHandler(NotFoundException.class)
+//	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+//		return new ErrorDto(notFoundException.getMessage());
+//	}
 }
---
+++ src/main/java/com/cooksys/twitterclone/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cooksys.twitterclone.services.impl;
+
+import org.springframework.stereotype.Service;
+import com.cooksys.twitterclone.services.ValidateService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl implements ValidateService{
+
+}
