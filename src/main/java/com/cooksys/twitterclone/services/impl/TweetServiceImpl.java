1211607829ded27fb017acf0afb820504070a0fc
Karanvir Sandhu <k2sandhu@ucsd.edu>
2023-08-31 13:35:10 -0700

    Pulling unmerged changes

--- src/main/java/com/cooksys/twitterclone/services/impl/TweetServiceImpl.java
+++ src/main/java/com/cooksys/twitterclone/services/impl/TweetServiceImpl.java
@@ -56,37 +56,12 @@ public class TweetServiceImpl implements TweetService {
     @Override
     public List<UserResponseDto> getUsersByTweetId(Long id) throws NotFoundException {
 
-//        Deleted users should be excluded from the response.
 
-        //Tweet foundTweet = tweetRepository.findById(id);
-    	
-//    	//List<Tweet> foundTweet = tweetRepository.findByLikedByUsers(id);
-//
-////        if(foundTweet.isEmpty() || foundTweet.get().isDeleted()) {
-//            throw new NotFoundException("Tweet not found");
-//        }
-
-        // List<User> foundUsers = tweetRepository.findByLikedByUsersAndDeleted(foundTweet.get().getLikedByUsers(), false);
-        // List<User> foundUsers = tweetRepository.findByLikedByUsersAndDeletedFalse(id);
-
-        // does not filter out deleted users currently
-        //return userMapper.entitiesToDtos(foundTweet.get().getLikedByUsers());
-        // return userMapper.entitiesToDtos(foundUsers);
-    	
-
-//        return userMapper.entitiesToDtos(foundTweet.get().getLikedByUsers());
-        // return userMapper.entitiesToDtos(foundUsers);
-
-        //return userMapper.entitiesToDtos(foundTweet.get().getLikedByUsers().stream().filter(user -> !user.isDeleted()).toList());
-    	
-    	return null;
     }
+  
+
     
-    public List<TweetResponseDto> getTweetLikes(){
-    	return null;
-        return userMapper.entitiesToDtos(foundTweet.get().getLikedByUsers().stream().filter(user -> !user.isDeleted()).toList());
-    }
-
+    
 
 	@Override
 	public List<HashtagDto> getHashtagByTweetId(Long id) throws NotFoundException {
