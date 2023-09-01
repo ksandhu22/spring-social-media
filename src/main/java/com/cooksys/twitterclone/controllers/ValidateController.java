809b49adc5fff7944381443782ce82cdb7bc3d3a
Karanvir Sandhu <k2sandhu@ucsd.edu>
2023-08-31 21:41:29 -0700

    Implemented Get available and Get exist endpoint

--- src/main/java/com/cooksys/twitterclone/controllers/ValidateController.java
+++ src/main/java/com/cooksys/twitterclone/controllers/ValidateController.java
@@ -1,14 +1,29 @@
 package com.cooksys.twitterclone.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.cooksys.twitterclone.services.ValidateService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+
+	private final ValidateService validateService;
+
+	@GetMapping("/username/available/@{username}")
+	public boolean availableUsername(@PathVariable String username) {
+		return validateService.availableUsername(username);
+	}
+
+	@GetMapping("/username/exists/@{username}")
+	public boolean usernameExists(@PathVariable String username) {
+		return validateService.usernameExists(username);
+	}
 
 }
